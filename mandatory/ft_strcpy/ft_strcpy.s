283999d09ff4d6291276f8f9dd7b009c0932c98c
SouleEater99 <rimox.midox2011@gmail.com>
2025-03-29 21:12:07 +0000

    [libasm] ====> Done

--- mandatory/ft_strcpy/ft_strcpy.s
+++ mandatory/ft_strcpy/ft_strcpy.s
@@ -3,21 +3,36 @@ section .text
 
 ft_strcpy:
     push    rbp
-    xor     rbp, rsp
+    mov     rbp,    rsp            ; create stack frame
 
-    mov     rbx, 0
+    push    rbx                 ; callee-saved registers
+    xor     rbx,    rbx            ; rbx = 0
+
+.check:
+    test    rdi,    rdi         ; check null in arguments
+    jz      .error
+    test    rsi,    rsi         ; check null in arguments
+    jz      .error
 
 .loop:
-    cmp     byte [rsi + rbx], 0
-    je     .end
-    mov     cl, [rsi + rbx]
-    mov     [rdi + rbx], cl
-    add     rbx, 1
+    mov     cl,     [rsi + rbx]
+    mov     [rdi + rbx],    cl
+    test    cl,     cl          ; Check for null terminator
+    jz      .end
+    inc     rbx                 ; Faster than add
     jmp     .loop
 
+
+
 .end:
-    mov     byte [rdi + rbx], 0
-    mov     rax, rdi
-    mov     rsp, rbp
-    pop     rbp
-    ret     
+    mov     rax,    rdi            ; intialize return value  which is rax
+
+.cleanup:
+    pop     rbx                 ; return calle-saved register to rbx
+    mov     rsp,    rbp            ; return rsp stack pointer to the base point place
+    pop     rbp                 ; return old rbp
+    ret
+
+.error:
+    xor     rax,    rax         ; Return NULL if invalid input
+    jmp     .cleanup
--- mandatory/ft_strcpy/main.c
+++ mandatory/ft_strcpy/main.c
@@ -10,6 +10,14 @@ int main()
     ft_strcpy(dest, s);
     printf("===================\n");
     for (int i = 0; s[i]; i++)
-        printf("%c", dest[i]);
+    printf("%c", dest[i]);
     printf("\ndest : %s\n", ft_strcpy(dest, s));
+    printf("===================\n");
+    if (!ft_strcpy(NULL, NULL))
+        printf("=========={:: NULL ::}=========\n");
+        
+    if (!ft_strcpy(NULL, NULL))
+        printf("=========={:: NULL ::}=========\n");
+    else
+        printf("=========={param1 = dest, param2 = NULL}=========\n");
 }
