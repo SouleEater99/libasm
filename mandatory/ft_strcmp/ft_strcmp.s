b4daac84dd4d35def66a2a0ebcbfc764df9d4388
SouleEater99 <rimox.midox2011@gmail.com>
2025-04-02 10:26:51 +0000

    [libasm/ft_strcmp] ======> ft_strcmp created

--- mandatory/ft_strcmp/ft_strcmp.s
+++ mandatory/ft_strcmp/ft_strcmp.s
@@ -1,7 +1,7 @@
 section .text
     global  ft_strcmp
 
-ft_strcmp:
+ft_strcmp:                              ; Creater stack frame and saved callee register too
     push    rbp
     mov     rbp,    rsp
 
@@ -9,26 +9,57 @@ ft_strcmp:
     xor     rbx,    rbx
 
 ; i will handel if the argument is NULL, even if it's not handled in standard strcmp().
-.check_args:
-    test    rdi,    rdi
-    jz      .error
-    test    rsi,    rsi
-    jz      .error
+.check_NULL:
+    cmp     rdi,    rsi                 ; Check if the two arguments is NULL.
+    mov     rax,    0
+    je      .cleanup
+
+    cm    rdi,    rdi                 ; Check the first argument if it's NULL
+    mov     rax,    -1
+    jz      .cleanup
+
+    test    rsi,    rsi                 ; Check the Second argument if it's NULL
+    mov     rax,    1
+    jz      .cleanup
+
+.check_null_terminator:                 ; Check everytime null terminator in both strings
+    mov     rax,    [rdi + rbx]
+    test    rax,    rax
+    jz      .end
+
+    mov     rax,    [rsi + rbx]
+    test    rax,    rax
+    jz      .end
 
 .loop:
-    mov     rax,    byte [rdi + rbx]
-    test    rax,    rax
-    jz      .
-    cmp     rax,    byte [rsi + rbx]
-    sub     rax,    byte [rsi + rbx]  
-    je      .loop
+    mov     rax,    [rdi + rbx]
+    cmp     rax,    [rsi + rbx]
+    jne      .end
+    inc     rbx
+    je      .check_null_terminator
 
-.cleanup:
+.end:                                   ; Set the final flag to return
+    mov     rax,    [rdi + rbx]
+    cmp     rax,    [rsi + rbx]
+    jg      .greater
+    jl      .less
+    je      .equal
+
+
+.cleanup:                               ; return saved register on the stack
     pop     rbx
     mov     rsp, rbp
     pop     rbp
     ret
 
-.error:
+.equal:
     mov     rax,    0
+    jmp     .cleanup
+
+.less:
+    mov     rax,    -1
+    jmp     .cleanup
+
+.greater:
+    mov     rax,    1
     jmp     .cleanup
--- mandatory/ft_strcmp/main.c
+++ mandatory/ft_strcmp/main.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-#include <string.h>
 
-// extern int ft_strcmp(const char *s1, const char *s2);
+extern int ft_strcmp(const char *s1, const char *s2);
 
 int main() {
-    printf("NULL vs NULL: %d (expected: 0)\n", strcmp(NULL, NULL));
-    printf("NULL vs 'abc': %d (expected: -1)\n", strcmp(NULL, "abc"));
-    printf("'abc' vs NULL: %d (expected: 1)\n", strcmp("abc", NULL));
-    printf("Equal strings: %d (expected: 0)\n", strcmp("hello", "hello"));
-    printf("s1 < s2: %d (expected: -)\n", strcmp("apple", "banana"));
-    printf("s1 > s2: %d (expected: +)\n", strcmp("zebra", "apple"));
+    printf("NULL vs NULL: %d (expected: 0)\n", ft_strcmp(NULL, NULL));
+    printf("NULL vs 'abc': %d (expected: -1)\n", ft_strcmp(NULL, "abc"));
+    printf("'abc' vs NULL: %d (expected: 1)\n", ft_strcmp("abc", NULL));
+    printf("Equal strings: %d (expected: 0)\n", ft_strcmp("hello", "hello"));
+    printf("s1 < s2: %d (expected: -)\n", ft_strcmp("apple", "banana"));
+    printf("s1 > s2: %d (expected: +)\n", ft_strcmp("zebra", "apple"));
     return 0;
 }
